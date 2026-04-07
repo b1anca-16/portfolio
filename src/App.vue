c181917832d6d8eaaa1a578a1783e146399bfce5
b1anca-16 <benzb@mailo.com>
2026-04-07 21:44:35 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Home />
+  </div>
+</template>
+
+<script setup>
+//später mit router-view arbeiten!
+import Home from './views/Home.vue'
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-purple-500 text-white p-10 text-center">Willkommen!</div>
+</template>
