8f827865b879ac87e07a219c05ee3d4007bd9aef
b1anca-16 <benzb@mailo.com>
2026-04-08 19:50:31 +0200

    initial Hero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="bg-purple-500 text-white p-10 text-center">Willkommen!</div>
+  <div class="bg-studio h-screen px-8 md:px-20 grid grid-cols-1 md:grid-cols-8 items-center gap-20">
+    <div class="col-span-2"></div>
+
+    <div class="col-span-2">
+      <img src="../../public/woman.png" alt="Bianca" class="rounded-full shadow-lg" />
+    </div>
+
+    <div class="col-span-4 text-right md:text-left">
+      <h1 class="text-peach text-5xl md:text-6xl font-bold mb-4">Hi, I'm Bianca</h1>
+      <p class="text-pearlstone text-lg md:text-xl max-w-lg">
+        Fullstack Web Developer & Online Media Student. I build interactive, modern websites with
+        Tailwind & Vue.
+      </p>
+    </div>
+  </div>
 </template>
