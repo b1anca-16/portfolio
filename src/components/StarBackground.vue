9058b515cd264ff4b538500e6e5746f9c485378b
b1anca-16 <benzb@mailo.com>
2026-04-13 16:05:27 +0200

    stars background

---
+++ src/components/StarBackground.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const stars = Array.from({ length: 150 }, () => ({
+  top: Math.random() * 200,
+  left: Math.random() * 100,
+  size: Math.random() * 3 + 1,
+  duration: Math.random() * 200 + 100,
+}))
+</script>
+
+<template>
+  <div class="absolute inset-0 overflow-hidden">
+    <div
+      v-for="(star, i) in stars"
+      :key="i"
+      class="absolute bg-white rounded-full animate-[floatStars_linear_infinite]"
+      :style="{
+        top: star.top + '%',
+        left: star.left + '%',
+        width: star.size + 'px',
+        height: star.size + 'px',
+        animationDuration: star.duration + 's',
+      }"
+    ></div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import Nav from '../components/Nav.vue'
 import Hero from '../components/Hero.vue'
+import StarBackground from '../components/StarBackground.vue'
 </script>
 
 <template>
-  <div class="bg-martinique h-screen">
-    <div class="max-w-6xl mx-auto px-6 md:px-10">
+  <div class="bg-martinique min-h-screen relative overflow-hidden">
+    <StarBackground />
+    <div class="relative z-10 max-w-6xl mx-auto px-6 md:px-10">
       <Nav />
-      <div class="h-[calc(100vh-80px)] flex-1 flex items-center">
+      <div class="h-[calc(100vh-80px)] flex items-center">
         <Hero />
       </div>
     </div>
